4c7de9284dea838df0eca4aafae1c72891d8cea2
Jacob Harasimo <jacobharasimo@gmail.com>
2017-03-23 12:26:48 -0400

    fix default route

--- src/config/router/router.config.ts
+++ src/config/router/router.config.ts
@@ -6,6 +6,10 @@ export class RouterConfig {
 
     constructor($stateProvider: ng.ui.IStateProvider,
                 $urlRouterProvider: ng.ui.IUrlRouterProvider) {
+        // pass through / to main state
+        $urlRouterProvider.when('', '/bigData/');
+        // unrecognized routes go to error state
+        $urlRouterProvider.otherwise('/404');
         $stateProvider
             .state('app', {
                 url: '/main',
