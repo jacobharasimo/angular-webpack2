2394615ea14712693856a04a0a6584be09fd0c05
Jacob Harasimo <jacobharasimo@gmail.com>
2017-03-23 16:50:03 -0400

    use vs-repeat so shoudl work better on IE

--- src/components/explorer/explorer.module.ts
+++ src/components/explorer/explorer.module.ts
@@ -1,12 +1,13 @@
 import * as angular from 'angular';
-
+import 'angular-vs-repeat';
 import './explorer.scss';
 import { ExplorerComponent } from './explorer.component';
 import { TreeNodeModule } from './tree-node';
 
 export const ExplorerModule = angular.module('explorer',
     [
-        TreeNodeModule
+        TreeNodeModule,
+        'vs-repeat'
     ])
     .component('explorer', ExplorerComponent)
     .name;
